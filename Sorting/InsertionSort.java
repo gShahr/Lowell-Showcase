1e3b3247102d6eea5a9ee5b3cf35b251dfddc6c0
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-06-08 21:45:46 -0400

    Create InsertionSort.java

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class SelectionSort {
+  public static void main(String[] args) {
+    // create a console for the user to input response
+    Scanner console = new Scanner(System.in);
+    int n = console.nextInt();
+    int[] array = new int[n];
+    for (int i = 0; i < n; i ++) {
+      array[i] = console.nextInt();
+    }
+    // start the index at 1 because the first element is seen as sorted 
+    for (int i = 1; i < array.length; i++) {
+      // create an index j that accounts for the first element of the sorted pile that goes up until it hits the unsorted pile
+      int j = i - 1;
+      // We keep going until we hit the 0th element or the start of the sorted pile to find where the i index of array fits in 
+      // the program pushes up elements foward until it finds an element smaller than value of array at index i (or until it j hits the 0th element, which means that it replaces the first element of the array)
+      while (j >= 0 && array[j] > array[i]) {
+        // pushes up elements by one until it reaches the beginning of the array or when the element being inserted is bigger than a value in the sorted section of the array
+        array[j+1] = array[j];
+        // move j back everytime so that it reaches beginning of sorted array
+        j--;
+      }
+      // have to add one to account for the extra one we substract from the above while loop and insert our next value into that location 
+      array[j+1] = array[i];
+    }
+    
+    // print out the values to the console with a space in between them
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
+    }
+  }
+}
+// Divides array into a sorted and unsorted array
+// O(n^2) 
