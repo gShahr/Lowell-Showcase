0d2f17e514aec34a3b09f773bdb570bcae77ddbf
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-06-10 23:32:16 -0400

    Create bleep.cpp

---
+++ mini-project/bleep.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "functions.hpp"
+
+int main() {
+  std::string word = "broccoli";
+  std::string text = "broccoli is what its name suggest whitch that is that it is broccoli.";
+  bleep(word, text);
+  std::cout << text;
+
+}
