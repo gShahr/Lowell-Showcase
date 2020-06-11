468fb3b11906bc3453960eabb9840d6ca32dc0be
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-06-10 23:41:38 -0400

    Update bleep.cpp

--- mini-project/bleep.cpp
+++ mini-project/bleep.cpp
@@ -6,6 +6,7 @@ int main() {
   std::string word = "broccoli";
   std::string text = "broccoli is what its name suggest whitch that is that it is broccoli.";
   bleep(word, text);
-  std::cout << text;
-
+  for (int i = 0; i < text.size(); i++) {
+    std::cout << text[i];
+  }
 }
