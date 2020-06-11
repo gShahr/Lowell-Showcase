254119d7a82169c7f9fa42db3adcafb3fab3b282
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-06-10 23:33:01 -0400

    Create functions.cpp

---
+++ mini-project/functions.cpp
@@ -0,0 +1,10 @@
+#include "functions.hpp"
+#include <string>
+
+// define functions
+void bleep(std::string word, std::string &text) {
+  std::string text2 = text;
+  for (int i = 0; i < text.length(); i++) {
+    
+  }
+}
