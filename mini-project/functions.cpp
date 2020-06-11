2da4966637c6405ed0c86a5d319fa709ca33def0
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-06-10 23:51:33 -0400

    Update functions.cpp

--- mini-project/functions.cpp
+++ mini-project/functions.cpp
@@ -5,6 +5,17 @@
 void bleep(std::string word, std::string &text) {
   std::string text2 = text;
   for (int i = 0; i < text.length(); i++) {
-    
+    int tracker = 0;
+    while (text[i] != ' ') {
+      if (text[i] == word[tracker]) {
+        i++;
+        tracker++;
+        if (tracker == word.size()) {
+          for (int i = 0; i < 8; i++) {
+            text[7-i] = '*';
+          }
+        }
+      }
+    }
   }
 }
