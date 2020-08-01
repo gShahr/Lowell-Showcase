32b1b9411b2dc7e226dc3fb43765bf1d09f6d48b
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-08-01 00:29:27 -0400

    Add files via upload

---
+++ Amherst_187/Assignment 10/heap-queues-student/src/structures/MinQueue.java
@@ -0,0 +1,125 @@
+package structures;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import comparators.ReverseIntegerComparator;
+
+public class MinQueue<V> implements PriorityQueue<Integer, V> {
+		
+	StudentArrayHeap<Integer, V> heap;
+	
+	public MinQueue() {
+		heap = new StudentArrayHeap<Integer, V>(new ReverseIntegerComparator());
+	}
+	
+	/** used this as a way to return a new instance of the objected each time a new value was added
+	public MinQueue(StudentArrayHeap<Integer, V> other) {
+		heap = other;
+	} */
+	
+	/**
+	 * Enqueues the specified {@code value} into this {@link PriorityQueue} with
+	 * the specified {@code priority}. This runs in O(log(size)) time. For
+	 * convenience this method returns the modified {@link PriorityQueue}.
+	 * @param <P>
+	 * @param <P>
+	 * 
+	 * @param priority
+	 *            the priority of this enqueue
+	 * @param value
+	 *            the value being enqueued
+	 * @return the modified {@link Priori tyQueue}.
+	 * @throws NullPointerException
+	 *             if {@code prioirty} is {@code null} or {@code value} is
+	 *             {@code null}.
+	 */
+	
+	public PriorityQueue<Integer, V> enqueue(Integer priority, V value) {
+		if (priority == null || value == null) {
+			throw new NullPointerException();
+		}
+		heap.add(priority, value); // add to heap
+		/* PriorityQueue<Integer, V> queue = new MinQueue(heap);  //create new object to account for added element
+		return queue; */
+		return this; // return an instance of the object 
+	}
+
+	/**
+	 * Removes the value with the highest priority in this {@link PriorityQueue}
+	 * and then returns it. This runs in O(log(size)) time.
+	 * 
+	 * @return the value with the highest priority in this {@link PrioirtyQueue}
+	 * @throws IllegalStateException
+	 *             if this {@link PriorityQueue} is empty.
+	 */
+	public V dequeue() {
+		if (isEmpty()) throw new IllegalStateException();
+		V value = heap.remove();
+		return value;
+	}
+
+	/**
+	 * Returns the value with the highest priority in this {@link PriorityQueue}.
+	 * 
+	 * @return the value with the highest priority in this {@link PriorityQueue}.
+	 * @throws IllegalStateException
+	 *             if this {@link PriorityQueue} is empty.
+	 */
+	public V peek() {
+		if (isEmpty()) throw new IllegalStateException();
+		V value = heap.peek();
+		return value;
+	}
+
+	/**
+	 * Returns an {@link Iterator} over all of the entries in this
+	 * {@link PriorityQueue}. The order of these elements is unspecified and a
+	 * call to {@link Iterator#remove()} results in an
+	 * {@link UnsupportedOperationException}.
+	 * 
+	 * @return an {@link Iterator} over all of the values in this
+	 *         {@link PriorityQueue}.
+	 */
+	public Iterator<Entry<Integer, V>> iterator() {
+		/** List<Entry<Integer, V>> list = heap.asList();
+		return list.iterator(); */
+		return heap.asList().iterator();
+	}
+
+	/**
+	 * Returns the {@link Comparator} that is used to determine the ordering of
+	 * {@link Entry}s in this {@link PriorityQueue}.
+	 * 
+	 * @return the {@link Comparator} that is used to determine the ordering of
+	 *         {@link Entry}s in this {@link PriorityQueue}.
+	 */
+	public Comparator<Integer> getComparator() {
+		return new ReverseIntegerComparator();
+	}
+
+	/**
+	 * Returns the total number of elements in this {@link PriorityQueue}
+	 * 
+	 * @return the total number of elements in this {@link PriorityQueue}
+	 */
+	public int size() {
+		return heap.size();
+	}
+
+	/**
+	 * Returns {@code true} if this {@link PrioirtyQueue} has no elements and
+	 * {@code false} otherwise.
+	 * 
+	 * @return {@code true} if this {@link PrioirtyQueue} has no elements and
+	 *         {@code false} otherwise.
+	 */
+	public boolean isEmpty() {
+		return heap.size() == 0;
+	}
+
+}
+
+
