a905ddda0a08587f5d028517232b6aea514a081d
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-08-03 17:13:02 -0400

    Update program1a.c

--- Computing 1/program 1/program1a.c
+++ Computing 1/program 1/program1a.c
@@ -1,3 +1,16 @@
+/***********************************************************************************************
+	Program:	program1a.c
+	Author:		Gabriel S.
+	Date:		August 8, 2020
+	Time Spent:	1 hour
+	Purpose:	The purpose of this program is to determine whether a meeting room is
+				below, at or above the maximum room capacity. The user enters both the 
+				number of people in the room and the maximum number of people allowed,
+				and the program then determines whether the meeting is legal or not. 
+				It then prints to the screen how many more people can be in the room or 
+				how many people must leave the room for the meeting to be held.
+************************************************************************************************/
+
 // this is the standard input output library
 #include <stdio.h>
 
