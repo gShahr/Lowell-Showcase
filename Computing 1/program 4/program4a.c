1063b99cac8857badbbf6116934f4c9e95372303
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-08-04 10:16:02 -0400

    Update program4a.c

--- Computing 1/program 4/program4a.c
+++ Computing 1/program 4/program4a.c
@@ -1,7 +1,7 @@
 /***********************************************************************************************
 	Program:	  program2a.c
 	Author:		  Gabriel S.
-	Date:		  August 3, 2020
+	Date:		  August 4, 2020
 	Time Spent:	  1 hour
 	Purpose:	  The purpose of this program is to ask the user if they want to convert a 
                           length or a weight. It will only run if the user selects a 0, 1, or 2. 
