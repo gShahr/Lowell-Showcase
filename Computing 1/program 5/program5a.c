6a55b67420bbff40875a27b6d76ca16979c31558
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-08-05 19:36:26 -0400

    Update program5a.c

--- Computing 1/program 5/program5a.c
+++ Computing 1/program 5/program5a.c
@@ -1,3 +1,13 @@
+/***********************************************************************************************
+	Program:	  program2a.c
+	Author:		  Gabriel S.
+	Date:		  August 4, 2020
+	Time Spent:	  1 hour
+	Purpose:	  The purpose of this program is to ask the user if they want to convert a 
+                          length or a weight. It will only run if the user selects a 0, 1, or 2. 
+                          It should run until the user enters '0' to quit the program.
+************************************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 
