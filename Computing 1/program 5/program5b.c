a4aee3eff033be934407290cf246d012a24508ae
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-08-06 22:24:45 -0400

    Update program5b.c

--- Computing 1/program 5/program5b.c
+++ Computing 1/program 5/program5b.c
@@ -4,10 +4,10 @@
 #include <ctype.h>  // included so I can use isspace() to check for whitespaces
 
 // Preconditions
-   // 1. Quiz file must exist
-   // 2. Name must be less than 20 characters
-   // 3. Student must have 10 or less quiz scores
-   // 4. Nothing else besides name and quiz scores should be on the files
+   // 1. Quiz file must exist (Program checks this)
+   // 2. Name must be less than 20 characters (Program checks this)
+   // 3. Student must have 10 or less quiz scores (Program does not account for this)
+   // 4. Nothing else besides name and quiz scores should be on the files (Program does not account for this)
 
 // Global variables
 int lines = 0; // Counts how many students there are
@@ -44,19 +44,19 @@ void is_valid(FILE *input1, FILE *output) {
     output = fopen("averages.txt", "mode");
   }
 
-  char line[70]; // 22 character char array equals a 20 character name field since last element is \0, which tells it when to stop and since we need to check for the white space after the last name, which is an extra character since that is not included in the name -> therefore the maximum size of first name & space & last name is 20 characters.
-  int white_space = 0; // used to count number of white spaces
+  char line[70]; // had to make my array bigger so that it would read the whole line rather than just a part of it since it wouldn't go to the next line when using fgets()
+  // *old* 22 character char array equals a 20 character name field since last element is \0, which tells it when to stop and since we need to check for the white space after the last name, which is an extra character since that is not included in the name -> therefore the maximum size of first name & space & last name is 20 characters.
+  int white_space = 0; // used to count number of single spaces
 
   while (fgets(line, sizeof(line), input1)) {
 
-  printf("%c", line[0]); // used to debug
+  // printf("%c", line[0]); // used to debug
 
   if (isspace(line[0])) { // if there is a space, break out
     break;
   }
-
   // iterate through string line to count how many whitespaces there are in order to determine if it is a valid name
-  for (int i = 0; i < sizeof(line); i++) {
+  for (int i = 0; i < 21; i++) { // Go to 21 indices because \0 is not until the end -> therefore we need 20+1 to check the whitespace after the maximum number of characters 
     if (line[i] == ' ') {
       white_space++; // increment everytime there is a white space
     }
@@ -66,14 +66,11 @@ void is_valid(FILE *input1, FILE *output) {
   }
   // if there are less than 2 whitespaces, it means there is an extremely long name (either first, last, or both parts of the name) since a valid name will be short enough that 20 characters should be able to traverse through both parts of it, which will result in there being 2 or more white spaces
   if (white_space < 2) {
-    printf("Invalid Name!"); // invalid name due to it being too long (error message)
+    printf("Invalid Name on Line %d!", (lines+1)); // invalid name due to it being too long (error message and the line where the error occured)
     exit(1); // exit the program
   }
   lines++; // increment lines by one to indiciate that there is another student
-  }
-
-  for (int i = 0; i < 22; i++) {
-    line[i] = ' ';
+  white_space = 0; // reset value of white_space each iteration
   }
 }
 
@@ -89,7 +86,7 @@ void format_data(FILE *input1, FILE *output) {
   fprintf(output, "%-20s %-38s %-11s", "Name(s)", "Quiz Scores", " Average   \n"); // trial and error to make sure that each field is the required length (made a 1 space buffer between name and quiz sections)
   fprintf(output, "---------------------------------------------------------------------------- \n");
 
-  printf("\nThere are %d students", lines); // used to debug
+  printf("\nThere are %d student(s)", lines); // used to debug
 
   while (lines-- > 0) {
   char name[20]; // name of student that is 20 characters wide
@@ -136,4 +133,5 @@ void format_data(FILE *input1, FILE *output) {
 /* Some Errors encountered
 Semantic error: When one of the quiz scores is 100, it adds another line or adds it to everyone quiz scores. Sometimes registering one of the numbers at the end of one student's as a new line and sometimes not going to the next line.
 -> Fixed issue with why scores were being carried over to next student (had to reset values to 0 in score array)
+-> Had to reset the white_space value (included it outside of while loop by accident) -> Also had to make my line array bigger so I could successfully check the entire line and move to the next rather than limtiing it 22 characters and having the possbility of it still containg values from the previous lines
 */
