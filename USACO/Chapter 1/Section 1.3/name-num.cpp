3a9ba6414d81a8463dd6be9dd3f6656e92d873e6
Gabriel <62683642+gShahr@users.noreply.github.com>
2020-08-18 22:50:08 -0400

    Update name-num.cpp

--- USACO/Chapter 1/Section 1.3/name-num.cpp
+++ USACO/Chapter 1/Section 1.3/name-num.cpp
@@ -9,35 +9,33 @@ using namespace std;
 int main() {
   ifstream input("namenum.in"); ifstream dict("dict.txt");ofstream output("namenum.out");
   string num; input >> num;
-  vector<string> names;
   vector<vector<char>> letters = {{'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}, {'J', 'K', 'L'}, {'M', 'N', 'O'}, {'P', 'R', 'S'}, {'T', 'U', 'V'}, {'W', 'X', 'Y'}};
   string name;
+  int count = 0;
+  bool cond;
+  bool cond2;
+  int i = 0;
   while (dict >> name) {
     if (name.length() == num.length()) {
-      names.push_back(name);
-    }
-  }
-  bool cond;
-  for (int i = 0; i < names.size(); i++) {
-    for (int m = 0; m < num.length(); m++) {
-      cond = false;
-      int n = stoi(num.substr(m, 1));
-      for (int j = 0; j < 3; j++) {
-        if (names[i].substr(m, 1).compare(string(1, letters[n-2][j])) == 0) {
-          cond = true;
-          break;
+      cond2 = true;
+      for (int m = 0; m < num.length(); m++) {
+        cond = false;
+        int n = stoi(num.substr(m, 1));
+        int j;
+        for (j = 0; j < letters[n-2].size(); j++) {
+          if (name.substr(m, 1).compare(string(1, letters[n-2][j])) == 0) {
+            cond = true;
+          }
         }
+        if (!cond) {cond2 = false;}
       }
-      if (!cond) {names.erase(names.begin() + i); i--; break;}
+      if (cond2) {output << name << endl; count++;}
     }
+    i++;
   }
-  if (names.size() == 0) {
+  if (count == 0) {
     output << "NONE" << endl;
-  } else {
-    for (int i = 0; i < names.size(); i++) {
-      output << names[i] << endl;
-    }
-  }
+  } 
   input.close(); dict.close(); output.close();
   return 0;
 }
